fa18f3a8683f82dd63ae733a71917595ac537035
vaishnavihole <vaishnavihole1@gmail.com>
2023-03-02 11:10:50 +0530

    listen to port

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const  redis = require('redis');
 
 const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT);
+
+const app = express()
+
+app.listen(5000, ()=> {
+    console.log('App listening on port 3000📦')
+});
