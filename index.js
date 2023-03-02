6555dbe4b5bd1466e287962622b2b5a7290a24ac
vaishnavihole <vaishnavihole1@gmail.com>
2023-03-02 11:08:36 +0530

    port added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const  express = require('express');
 const fetch  = require('node-fetch');
 const  redis = require('redis');
+
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.PORT || 6379;
