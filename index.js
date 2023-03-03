5779526cf7c66843dddd0be3061a57d0f11355d0
vaishnavihole <vaishnavihole1@gmail.com>
2023-03-03 11:52:59 +0530

    use async await

--- index.js
+++ index.js
@@ -14,14 +14,17 @@ const client =  redis.createClient({
 const app = express();
 
 // make request to Github for data
-function getRepos(req, res, next) {
+async function getRepos(req, res, next) {
     try{
         console.log('Fetching Data...');
 
     const  { username } = req.params;
 
-    
+    const response = await fetch(`https://api.github.com/users/${username}`)
 
+    const data = await response.json();
+
+    res.send(data);
     }catch (err) {
         console.log(err);
         res.status(500)
